06f8c244f1dc5e099bbacc67a50e2eef93e7f9d9
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-23 16:51:17 +0530

    Update streamlit_app.py
    Multiselect was misspelled

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let us create a pick list so they can pick the fruit the want to include .. we are prepopulating values for giving example to customer.
 #streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index),['Avocado','Strawberries'])
 
-fruits_selected = streamlit.multselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display the table on the page
