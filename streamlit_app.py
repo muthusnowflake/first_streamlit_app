9db51076ceca31c048c1eccc062508ea05055fbd
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-30 02:58:49 +0530

    Update streamlit_app.py
    URLLIB Command has been also added after the import commands

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
