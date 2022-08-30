cfae36b1b2b43d609dee32b40e1ee65e2b9e7cc3
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-30 18:09:02 +0530

    Work
    Work

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
@@ -26,18 +25,26 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#streamlit.write('The user entered ', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)    
+    streamlit.dataframe(fruityvice_normalized)
+          
+          
+#import requests
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+# write your own comment - what does this do?
 
 
 #dont execute past this till we troubleshoot
+#streamlit.stop()
 streamlit.stop()
+
 #import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
