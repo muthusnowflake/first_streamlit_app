096b8741e5cf11e8ddfd5b8c2360d3cb3201bc7d
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-30 20:40:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_selected = streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display the table on the page
 #streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
