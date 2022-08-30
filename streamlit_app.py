9654cd0ff0cbb098b064cdfa58fb0a0dd7d87f2f
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-30 18:20:03 +0530

    Update streamlit_app.py
    Trial to work 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,36 @@
-
 #Streamlit Python Library has been imported
+
 import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
 
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#Display the table on the page
-#streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
+#Pandas Python Library has been imported
+import pandas
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-          streamlit.write('The user entered ', fruit_choice)
-    else:
-          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
 
- #except URLError as e:
- #         streamlit.error()
-
-
-
+import requests
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
 #dont execute past this till we troubleshoot
 streamlit.stop()
+
+import snowflake.connector
+
+
 #import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
