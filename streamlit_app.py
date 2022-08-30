d0725ee4390c7bfdfa579901bce9d29fb357349b
muthusnowflake <111734358+muthusnowflake@users.noreply.github.com>
2022-08-30 16:47:57 +0530

    Update streamlit_app.py
    Rolling back to original working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+1,9 +1,10 @@
+#Streamlit Python Library has been imported
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -15,34 +24,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display the table on the page
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-          streamlit.write('The user entered ', fruit_choice)
-    else:
-          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          streamlit.dataframe(fruityvice_normalized)
-
- except URLError as e:
-    streamlit.error()
-
-
-
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #streamlit.text(fruityvice_response.json())
-
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
-
 #dont execute past this till we troubleshoot
 streamlit.stop()
 #import snowflake.connector
